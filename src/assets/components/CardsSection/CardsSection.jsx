77a229a6a2deb79bb2afec22f307d9a9a36d1ff8
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-02-13 16:48:15 -0700

    Card Section

--- src/assets/components/CardsSection/CardsSection.jsx
+++ src/assets/components/CardsSection/CardsSection.jsx
@@ -3,14 +3,21 @@ import React from 'react'
 import styles  from './CardsSection.module.css';
 import CardPrimary from '../CardPrimary/CardPrimary';
 import CardSecondary from '../CardSecondary/CardSecondary';
+import { Bar } from '../Bar/Bar';
+import { Link } from 'react-router-dom';
 
 
-function CardsSection({title}) {
+function CardsSection({title, link}) {
   return (
-    <div className={`container`}>
-      <h3>{title}</h3>
+    <div className={`${styles.cardsSection} flex container`}>
+      <h3 className={`${styles.title} text-uppercase`}>{title}</h3>
       <CardPrimary />
       <CardSecondary />
+      <CardSecondary />
+      <CardSecondary />
+      <CardSecondary />
+      <CardSecondary />
+      <button type="button" className={`btn btn-dark `}><Link to={link} className={`${styles.btn}`}>Ver Mas</Link></button>
     </div>
   )
 }
--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -6,8 +6,8 @@ export function Home() {
     return (
         <div className={styles.home}>
             <Banner />
-            <CardsSection title='Blog'/>
-            <CardsSection title='Youtube'/>
+            <CardsSection title='Blog' link='/blog'/>
+            <CardsSection title='Youtube' link='/youtube'/>
         </div>
     )
 }
