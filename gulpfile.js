5d543f59a51d94d801d479b8c55c855048734628
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-02-15 18:08:58 -0700

    Agregada funcionalidad de convertir Avif y Webp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const { src, dest, parallel } = require('gulp')
+
+// Imagnes
+const cache    = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp     = require('gulp-webp');
+const avif     = require('gulp-avif')
+
+function imageMin(done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/assets/pictures/img/*{png,jpg}')
+        .pipe(cache(imagemin(opciones)))        // Convertir a Webp
+        pipe(dest('src/assets/pictures/img'))   // Asignar donde se guardan los archivos
+
+    done()
+}
+
+function versionWebp(done) {
+    
+    const opciones = {
+        quality: 50,
+    };
+
+    
+    src('src/assets/pictures/img/*.{png,jpg}')  // Ubicacion para encontar los archivos
+        .pipe(webp(opciones))                   // Convertir a Webp
+        .pipe( dest('src/assets/pictures/img')) // Asignar donde se guardan los archivos
+
+    done();
+}
+
+
+function versionAvif(done) {
+    
+    const opciones = {
+        quality: 50,
+    };
+
+    
+    src('src/assets/pictures/img/*.{png,jpg}')  // Ubicacion para encontar los archivos
+        .pipe(avif(opciones))                   // Convertir a Webp
+        .pipe( dest('src/assets/pictures/img')) // Asignar donde se guardan los archivos
+
+    done();
+}
+
+
+
+
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.imageMin    = imageMin;
+exports.dev         = parallel(versionWebp, versionAvif)
+ 
