c87cdfbe226eaa55efb64aec3cae80786c1e306f
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-02-16 17:33:17 -0700

    Blog nuevo

--- src/assets/pages/Blogs/Chatgpt.jsx
+++ src/assets/pages/Blogs/Chatgpt.jsx
@@ -22,7 +22,7 @@ function chatgpt() {
           <h2 className='text-center'>La respuesta a tus preguntas: ChatGPT</h2>
           <Bar />
 
-          <picture>
+          <picture className={`${styles.picture}`}>
             <source srcSet={imgAvif} type='image/avif'/>
             <source srcSet={imgWebp} type='image/webp'/>
             <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg} alt='ChatGPT' />
@@ -31,7 +31,7 @@ function chatgpt() {
           <p>Imagina que estás en casa una noche, después de un día largo y agotador en el trabajo. Tienes una pregunta que te ronda la cabeza y no puedes encontrar la respuesta. ¿Qué haces en una situación así? Por supuesto, te metes en internet y empiezas a buscar. Pero hay tantos resultados y tantas páginas que es difícil saber por dónde empezar. ¿Te suena familiar?</p>
           <p>Pues imagina que en ese momento tienes a mano el chat de OpenAI. Con solo unos pocos clics, puedes preguntar cualquier cosa que quieras y recibir una respuesta precisa y confiable en poco tiempo. Es así de fácil.</p>
 
-          <picture>
+          <picture className={`${styles.picture}`}>
             <source srcSet={imgAvif2} type='image/avif'/>
             <source srcSet={imgWebp2} type='image/webp'/>
             <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg2} alt='ChatGPT3' />
@@ -40,7 +40,7 @@ function chatgpt() {
           <p>Pero, ¿cómo es posible esto? Bueno, detrás del chat de OpenAI se encuentra ChatGPT, una red neuronal entrenada con millones de textos y conversaciones. Esta tecnología avanzada de lenguaje natural permite a ChatGPT comprender el contexto de las preguntas y producir respuestas coherentes y precisas.</p>
           <p>Y no solo es útil para investigaciones serias, también puedes usar el chat de OpenAI para experimentar y tener un poco de diversión. Haz preguntas divertidas y desafiantes y verás cómo ChatGPT responde. Será una forma divertida de aprender sobre la tecnología y la inteligencia artificial en general.</p>
 
-          <picture>
+          <picture className={`${styles.picture}`}>
             <source srcSet={imgAvif3} type='image/avif'/>
             <source srcSet={imgWebp3} type='image/webp'/>
             <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg3} alt='Open AI' />
--- src/assets/pages/Blogs/RealidadVirtual.jsx
+++ src/assets/pages/Blogs/RealidadVirtual.jsx
@@ -1,17 +1,65 @@
 import React from 'react';
+import { Bar } from '../../components/Bar/Bar';
 
 import styles from './Blogs.module.css';
 
+import imgJpg1 from '../../pictures/img/realidad-virtual-1.jpg';
+import imgAvif1 from '../../pictures/img/realidad-virtual-1.avif';
+import imgWebp1 from '../../pictures/img/realidad-virtual-1.webp';
+
+import imgJpg2 from '../../pictures/img/realidad-virtual-2.jpg';
+import imgAvif2 from '../../pictures/img/realidad-virtual-2.avif';
+import imgWebp2 from '../../pictures/img/realidad-virtual-2.webp';
+
+import imgJpg3 from '../../pictures/img/realidad-virtual-3.jpg';
+import imgAvif3 from '../../pictures/img/realidad-virtual-3.avif';
+import imgWebp3 from '../../pictures/img/realidad-virtual-3.webp';
+
+import imgJpg4 from '../../pictures/img/realidad-virtual-4.jpg';
+import imgAvif4 from '../../pictures/img/realidad-virtual-4.avif';
+import imgWebp4 from '../../pictures/img/realidad-virtual-4.webp';
+
 function RealidadVirtual() {
   return (
-    <div className={`${styles.blogs} container`}>
-        <h2>La Realidad Virtual y Aumentada: El Futuro de la Entretención y la Tecnología</h2>
-        <p>La tecnología de realidad virtual (VR) y realidad aumentada (AR) está transformando la forma en que nos entretenemos y experimentamos el mundo que nos rodea. Desde videojuegos hasta la formación, estas tecnologías están ofreciendo experiencias únicas e inmersivas que no se pueden encontrar en ningún otro lugar.</p>
-        <p>La realidad virtual es una tecnología en la que se crea un ambiente artificial y se utiliza un dispositivo para sumergir al usuario en ese ambiente. Con la VR, los usuarios pueden interactuar con el entorno de manera natural, utilizando controladores de movimiento y otros dispositivos para navegar y experimentar.</p>     
-        <p>Por otro lado, la realidad aumentada es una tecnología que combina el mundo real con elementos digitales para crear una experiencia enriquecedora. Con la AR, los usuarios pueden ver objetos digitales superpuestos en el mundo real, lo que les permite interactuar con ellos de manera más natural.</p>    
-        <p>Estas tecnologías están teniendo un impacto significativo en el sector del entretenimiento. Los videojuegos de VR están ofreciendo experiencias únicas e inmersivas que nunca antes se han visto, y la AR está transformando la forma en que los usuarios interactúan con los juegos y las aplicaciones.</p>
-        <p>Además de los videojuegos, la VR y la AR también están teniendo un impacto significativo en la formación. Por ejemplo, la VR se está utilizando para crear simulaciones realistas que permiten a los estudiantes aprender de manera más efectiva. Y la AR está permitiendo a los profesores y los estudiantes interactuar con materiales educativos de manera más dinámica y enriquecedora.</p>
-        <p>En conclusión, la realidad virtual y aumentada son tecnologías con un gran potencial y están teniendo un impacto significativo en la forma en que nos entretenemos y experimentamos el mundo que nos rodea. A medida que estas tecnologías continúen evolucionando, es probable que veamos aún más aplicaciones innovadoras y sorprendentes en el futuro cercano.</p>
+    <div className={`${styles.containerBlogs} flex container`}>
+      <div className={`${styles.blogs} flex`}>
+          <h2 className='text-center'>La Realidad Virtual y Aumentada: El Futuro de la Entretención y la Tecnología</h2>
+          <Bar />
+
+          <picture className={`${styles.picture}`}>
+            <source srcSet={imgAvif1} type='image/avif'/>
+            <source srcSet={imgWebp1} type='image/webp'/>
+            <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg1} alt='Realidad Virtual' />
+          </picture>
+
+          <p>La tecnología de realidad virtual (VR) y realidad aumentada (AR) está transformando la forma en que nos entretenemos y experimentamos el mundo que nos rodea. Desde videojuegos hasta la formación, estas tecnologías están ofreciendo experiencias únicas e inmersivas que no se pueden encontrar en ningún otro lugar.</p>
+
+          <picture className={`${styles.picture}`}>
+            <source srcSet={imgAvif2} type='image/avif'/>
+            <source srcSet={imgWebp2} type='image/webp'/>
+            <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg2} alt='Realidad Virtual' />
+          </picture>
+
+          <p>La realidad virtual es una tecnología en la que se crea un ambiente artificial y se utiliza un dispositivo para sumergir al usuario en ese ambiente. Con la VR, los usuarios pueden interactuar con el entorno de manera natural, utilizando controladores de movimiento y otros dispositivos para navegar y experimentar.</p>     
+          <p>Por otro lado, la realidad aumentada es una tecnología que combina el mundo real con elementos digitales para crear una experiencia enriquecedora. Con la AR, los usuarios pueden ver objetos digitales superpuestos en el mundo real, lo que les permite interactuar con ellos de manera más natural.</p>    
+          <p>Estas tecnologías están teniendo un impacto significativo en el sector del entretenimiento. Los videojuegos de VR están ofreciendo experiencias únicas e inmersivas que nunca antes se han visto, y la AR está transformando la forma en que los usuarios interactúan con los juegos y las aplicaciones.</p>
+
+          <picture className={`${styles.picture}`}>
+            <source srcSet={imgAvif3} type='image/avif'/>
+            <source srcSet={imgWebp3} type='image/webp'/>
+            <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg3} alt='Realidad Virtual' />
+          </picture>
+
+          <p>Además de los videojuegos, la VR y la AR también están teniendo un impacto significativo en la formación. Por ejemplo, la VR se está utilizando para crear simulaciones realistas que permiten a los estudiantes aprender de manera más efectiva. Y la AR está permitiendo a los profesores y los estudiantes interactuar con materiales educativos de manera más dinámica y enriquecedora.</p>
+
+          <picture className={`${styles.picture}`}>
+            <source srcSet={imgAvif4} type='image/avif'/>
+            <source srcSet={imgWebp4} type='image/webp'/>
+            <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg4} alt='Realidad Virtual' />
+          </picture>
+
+          <p>En conclusión, la realidad virtual y aumentada son tecnologías con un gran potencial y están teniendo un impacto significativo en la forma en que nos entretenemos y experimentamos el mundo que nos rodea. A medida que estas tecnologías continúen evolucionando, es probable que veamos aún más aplicaciones innovadoras y sorprendentes en el futuro cercano.</p>
+      </div>
     </div>
   )
 }
