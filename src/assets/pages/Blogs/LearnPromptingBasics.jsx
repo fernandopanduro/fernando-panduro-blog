3cc3bb32718cdd20b8a9357233f48c63c1e27ad7
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-04-01 07:28:33 -0700

    eliminado exeso de emojis

--- src/assets/pages/Blogs/LearnPromptingBasics.jsx
+++ src/assets/pages/Blogs/LearnPromptingBasics.jsx
@@ -32,7 +32,7 @@ export default function LearnPromptingBasics() {
               <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg5} alt='ChatGPT' />
             </picture>
 
-            <p>¡Perfecto! 🎉 Comencemos con los fundamentos de la Inteligencia Artificial (IA) 🧠. La IA es un algoritmo inteligente que piensa como los humanos 👥. Es útil para automatizar tareas repetitivas, pero muchas veces necesita que un humano la dirija y le dé instrucciones 🤖💭.</p>
+            <p>¡Perfecto! 🎉 Comencemos con los fundamentos de la Inteligencia Artificial (IA) 🧠. La IA es un algoritmo inteligente que piensa como los humanos 👥. Es útil para automatizar tareas repetitivas, pero muchas veces necesita que un humano la dirija y le dé instrucciones.</p>
 
             <p>Una forma de instruir a una IA para que realice una tarea es a través del proceso de Promoting 🚀. El Promoting es simplemente darle instrucciones a la IA para que realice una tarea específica. Un ejemplo de Promoting es asignarle un rol a la IA, por ejemplo, "Eres un médico" o "Eres un abogado" 👨‍⚕️👨‍⚖️.</p>
 
@@ -42,11 +42,11 @@ export default function LearnPromptingBasics() {
               <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg3} alt='ChatGPT' />
             </picture>
 
-            <p>Existen diferentes tipos de Prompts 📋, como los estándares, que son aquellos que únicamente le hacen una pregunta a la IA, como "¿Cuál es la capital de Francia?" 🗼 También existen los Few Shot Standard Prompts, que contienen el ejemplo en el input que se introduce, como "¿Cuál es la capital de España? - Madrid ¿Cuál es la capital de Italia? - Roma" 🇪🇸🇮🇹.</p>
+            <p>Existen diferentes tipos de Prompts, como los estándares, que son aquellos que únicamente le hacen una pregunta a la IA, como "¿Cuál es la capital de Francia?" 🗼 También existen los Few Shot Standard Prompts, que contienen el ejemplo en el input que se introduce, como "¿Cuál es la capital de España? - Madrid ¿Cuál es la capital de Italia? - Roma" 🇪🇸🇮🇹.</p>
 
-            <p>Los prompts pueden incluir contexto, instrucciones, ejemplos, preguntas, respuestas e incluso otros prompts 🤔. Al agregar contexto y ejemplos adicionales, podemos mejorar el rendimiento de las IAs 📈.</p>
+            <p>Los prompts pueden incluir contexto, instrucciones, ejemplos, preguntas, respuestas e incluso otros prompts. Al agregar contexto y ejemplos adicionales, podemos mejorar el rendimiento de las IAs 📈.</p>
 
-            <p>Otro aspecto importante de la IA es el chatbot con Inteligencia Artificial 🤖💬, un modelo que puede recordar el historial y contexto de la conversación. Los chatbots solo son valiosos si hay contexto y buenos prompts 🗣️. El ChatGPT es un ejemplo de un chatbot con IA que difiere del GPT-3 en su capacidad para recordar el historial y el contexto de la conversación 🤝.</p>
+            <p>Otro aspecto importante de la IA es el chatbot con Inteligencia Artificial, un modelo que puede recordar el historial y contexto de la conversación. Los chatbots solo son valiosos si hay contexto y buenos prompts 🗣️. El ChatGPT es un ejemplo de un chatbot con IA que difiere del GPT-3 en su capacidad para recordar el historial y el contexto de la conversación 🤝.</p>
 
             <picture className={`${styles.picture}`}>
               <source srcSet={imgAvif} type='image/avif'/>
@@ -54,7 +54,7 @@ export default function LearnPromptingBasics() {
               <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg} alt='ChatGPT' />
             </picture>
 
-            <p>Es posible modificar los prompts para mejorar la calidad de las respuestas de la IA 📝. Por ejemplo, darle un estilo coloquial, amistoso e informal a la respuesta, agregar descripciones con adjetivos, o utilizar la inducción preliminar para afectar la estructura y especificación de la respuesta 🤗.</p>
+            <p>Es posible modificar los prompts para mejorar la calidad de las respuestas de la IA 📝. Por ejemplo, darle un estilo coloquial, amistoso e informal a la respuesta, agregar descripciones con adjetivos, o utilizar la inducción preliminar para afectar la estructura y especificación de la respuesta.</p>
 
             <p>También es importante mencionar que existen diferentes tipos de IAs 🖼️🎶🎥, como las que generan texto, audio, música, imágenes e incluso videos. No todas las IAs están en un nivel útil para su uso e implementación en la vida cotidiana 🚫.</p>
 
@@ -64,7 +64,7 @@ export default function LearnPromptingBasics() {
               <img className={`${styles.img} rounded-1`} loading='lazy' width='200' height='300' src={imgJpg2} alt='ChatGPT' />
             </picture>
 
-            <p>En resumen, la IA es un algoritmo inteligente que piensa como los humanos y es útil para automatizar tareas repetitivas 🧠🤖. El Promoting es el proceso de instruir a una IA para que realice una tarea específica, y existen diferentes tipos de prompts para darle instrucciones a la IA 🚀📋. La IA también puede utilizarse en chatbots con IA, que pueden recordar el historial y el contexto de la conversación 🗣️💬. Es posible modificar los prompts para mejorar la calidad de las respuestas de la IA 🤔📝, y existen diferentes tipos de IAs para generar texto, audio, música, imágenes e incluso videos 🖼️🎶🎥.</p>
+            <p>En resumen, la IA es un algoritmo inteligente que piensa como los humanos y es útil para automatizar tareas repetitivas 🧠. El Promoting es el proceso de instruir a una IA para que realice una tarea específica, y existen diferentes tipos de prompts para darle instrucciones a la IA 📋. La IA también puede utilizarse en chatbots con IA, que pueden recordar el historial y el contexto de la conversación 🗣️💬. Es posible modificar los prompts para mejorar la calidad de las respuestas de la IA, y existen diferentes tipos de IAs para generar texto, audio, música, imágenes e incluso videos 🖼️🎶🎥.</p>
             
         </div>
     </div>
