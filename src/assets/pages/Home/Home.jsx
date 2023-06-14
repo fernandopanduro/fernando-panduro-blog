1b1345990ff1a2d611e9468d8ce2d70b2e0f0243
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-06-14 11:12:46 -0700

    Agregado loader

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -5,11 +5,34 @@ import CardsSectionYT from '../../components/CardsSectionYT/CardsSectionYT';
 import Portfolio from '../../components/Portfolio/Portfolio';
 import styles from './Home.module.css';
 import CardMe from '../../components/CardMe/CardMe';
+import { useState } from 'react';
 
+const Preload = () => {
+    return (
+        <div className={styles.preload}>
+            <div className={styles.threeBody}>
+              <div className={styles.threeBody__dot}></div>
+              <div className={styles.threeBody__dot}></div>
+              <div className={styles.threeBody__dot}></div>
+            </div>
+        </div>
+        
+    )
+}
 
 export function Home() {
+
+    const [ loading, setLoading ] = useState(true);
+
+    if (loading) {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000)
+    }
+
   window.scrollTo(0,0)
     return (
+        !loading ? (
         <div className={styles.home}>
             <Helmet><title>Fernando Panduro</title></Helmet>
             <Banner />
@@ -22,5 +45,6 @@ export function Home() {
             <Portfolio link='/portfolio'/>
             <CardsSectionYT heading='Youtube' />
         </div>
+        ) : <Preload />
     )
 }
